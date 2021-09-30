dacaf95ae5df42a68e110feb105b11b70d0f0bea
Nikola Simic <nikola_simic@msn.com>
2021-09-30 14:52:36 +0200

    Added base controller

---
+++ Thynk.Api/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Thynk.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseController : ControllerBase
+    {
+        public readonly ILogger<BaseController> _logger;
+
+        private IMediator _mediator;
+        protected IMediator Mediator
+        {
+            get
+            {
+                return _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+            }
+        }
+
+        public BaseController(ILogger<BaseController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
