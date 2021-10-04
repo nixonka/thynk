2b006393654c6a26240ee84f735abb0bf8a1587c
Nikola Simic <nikola_simic@msn.com>
2021-10-04 12:47:36 +0200

    Works better with repository :)

--- Thynk.Application/Employees/Commands/CreateEmployeeCommand.cs
+++ Thynk.Application/Employees/Commands/CreateEmployeeCommand.cs
@@ -2,30 +2,34 @@
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
+using Thynk.Application.Interfaces;
 using Thynk.Application.Wrappers;
 using Thynk.Domain;
 
 namespace Thynk.Application.Employees.Commands
 {
-    public partial class CreateEmployeeCommand : IRequest<Response<int>>
+    public partial class CreateEmployeeCommand : IRequest<Response<Employee>>
     {
         public string Name { get; set; }
     }
 
-    public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, Response<int>>
+    public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, Response<Employee>>
     {
         private readonly IMapper mapper;
+        private readonly IEmployeeRepositoryAsync employeeRepository;
 
-        public CreateEmployeeCommandHandler( IMapper mapper) {
+        public CreateEmployeeCommandHandler(IEmployeeRepositoryAsync employeeRepository, IMapper mapper) {
+            this.employeeRepository = employeeRepository;
             this.mapper = mapper;
         }
 
+        public IEmployeeRepositoryAsync EmployeeRepository { get; }
 
-
-        public async Task<Response<int>> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+        public async Task<Response<Employee>> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
         {
             var employee = mapper.Map<Employee>(request);
-            return new Response<int>(employee.Id);
+            await employeeRepository.AddAsync(employee);
+            return new Response<Employee>(employee);
         }
     }
 }
