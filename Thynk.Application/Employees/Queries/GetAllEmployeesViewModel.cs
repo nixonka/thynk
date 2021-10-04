e1b8243d89e15c0ab14809e46bc6716c82fc0746
Nikola Simic <nikola_simic@msn.com>
2021-10-04 13:29:48 +0200

    Expanded employee view model on backend

--- Thynk.Application/Employees/Queries/GetAllEmployeesViewModel.cs
+++ Thynk.Application/Employees/Queries/GetAllEmployeesViewModel.cs
@@ -4,5 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Job { get; set; }
+        public string Motto { get; set; }
+        public string Hobbies { get; set; }
+        public string Hometown { get; set; }
+        public string PersonalBlog { get; set; }
     }
 }
